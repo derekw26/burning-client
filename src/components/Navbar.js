118228a35c19aea281a8193e589d12e2f1c72ebe
Goutham <goutham@Goutham-2.local>
2021-11-11 09:35:59 +1100

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,10 @@ class Navbar extends Component{
       <div>
         <ul>
             <Link to="/"><li>Home</li></Link>
-<<<<<<< HEAD
             <Link to="/flights"><li>Flights</li></Link>
+            <Link to="/planes"><li>Planes</li></Link>
 
-=======
-            <Link to="/flights"><li>Create Flight</li></Link>
-            <Link to="/planes"><li>Create Plane</li></Link>
-            <Link to="/search"><li> Book Now</li></Link>
->>>>>>> 2b63f17c70c46e0c9ae48ffba3589e366954a289
+
 
         </ul>
       </div>
